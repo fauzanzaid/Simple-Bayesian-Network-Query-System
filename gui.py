424b043a46c97e676da66b75adf4d2656ef02530
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-11-21 16:24:03 +0530

    Create classes for text boxes

--- gui.py
+++ gui.py
@@ -39,10 +39,10 @@ class GUI(object):
 		self.P_PAD = 20
 		self.W_PAD = 50
 
-		self.TXT_BOX_HT = 20
-		self.TXT_BOX_WD = 100
-		self.TTL_BOX_HT = 20
-		self.TTL_BOX_WD = 100
+		self.TTL_BOX_HT = TitleBox.HT
+		self.TTL_BOX_WD = TitleBox.WD
+		self.TXT_BOX_HT = TextBox.HT
+		self.TXT_BOX_WD = TextBox.WD
 
 		self.P1_HT = self.LIN_SPC*3
 		self.P1_WD = 400
@@ -95,7 +95,10 @@ class GUI(object):
 
 		func, args = self.qu_cmd.get()
 		if func == "quit":
-			pass
+			self.scr.onclick(None)
+			self.scr.onkey(None, "q")
+			self.scr.bye()
+			return
 
 
 	def run(self):
@@ -110,3 +113,133 @@ class GUI(object):
 		self.ttl_base.pu()
 		self.ttl_base.speed(0)
 		self.ttl_base.tracer(0,0)
+
+		self.scr.ontimer(self.cmd_dispatcher, self.DISPATCH_DELAY)
+		self.scr.onclick(self.send_mouse_click)
+		self.scr.onkey(lambda:self.send_key_press("q"), "q")
+		self.scr.listen()
+
+		turtle.done()
+		self.send_key_press("q")
+
+
+
+class TextBox(object):
+	"""docstring for TextBox"""
+
+	WD = 100
+	HT = 20
+	PAD = 5
+	TXT_PAD = 2
+	COL = ((0,0,0),(0.9,0.9,0.9))
+
+	WDP = WD + PAD*2
+	HTP = HT + PAD*2
+
+
+	def __init__(self, ttl_base, cood_x, cood_y, text=""):
+		self.ttl_base = ttl_base
+		self.cood_x = cood_x
+		self.cood_y = cood_y
+		self.text = text
+
+
+	def draw_base(self):
+		old_color = self.ttl_base.color()
+		# old_size = self.ttl_base.pensize()
+
+		self.ttl_base.goto(self.cood_x, self.cood_y)
+
+		self.ttl_base.pd()
+		self.ttl_base.color(*self.COL)
+		# self.ttl_base.pensize(old_size)
+		self.ttl_base.fill(True)
+
+		self.ttl_base.seth(0)
+		self.ttl_base.fd(self.WD)
+		self.ttl_base.rt(90)
+		self.ttl_base.fd(self.HT)
+		self.ttl_base.rt(90)
+		self.ttl_base.fd(self.WD)
+		self.ttl_base.rt(90)
+		self.ttl_base.fd(self.HT)
+		self.ttl_base.rt(90)
+
+		self.ttl_base.fill(False)
+		# self.ttl_base.pensize(old_size)
+		self.ttl_base.color(*old_color)
+		self.ttl_base.pu()
+
+		self.write()
+
+
+	def write(self):
+		self.ttl_base.goto(self.cood_x+self.TXT_PAD, self.cood_y-self.HT+self.TXT_PAD)
+		self.ttl_base.write(self.text, font=("Mono", 8, "normal"))
+
+
+
+class TitleBox(TextBox):
+	"""docstring for TitleBox"""
+
+	WD = 150
+	HT = 30
+	PAD = 5
+	TXT_PAD = 7
+	COL = ((0,0,0),(0.8,0.8,1.0))
+
+
+	WDP = WD + PAD*2
+	HTP = HT + PAD*2
+
+	def __init__(self, ttl_base, cood_x, cood_y, text=""):
+		super(TitleBox, self).__init__(ttl_base, cood_x, cood_y, text)
+
+
+
+class ButtonBox(TextBox):
+	"""docstring for ButtonBox"""
+
+	COL = ((0,0,0),(0.7,0.8,0.7))
+	COL_ON = ((0,0,0),(0.8,1.0,0.8))
+
+	def __init__(self, ttl_base, cood_x, cood_y, text=""):
+		super(ButtonBox, self).__init__(ttl_base, cood_x, cood_y, text)
+		self.ttl_lit = turtle.Turtle()
+		self.ttl_lit.ht()
+		self.ttl_lit.pu()
+		self.ttl_lit.speed(0)
+		self.ttl_lit.tracer(0,0)
+
+
+	def on(self):
+		old_color = self.ttl_base.color()
+		# old_size = self.ttl_base.pensize()
+
+		self.ttl_base.goto(self.cood_x, self.cood_y)
+
+		self.ttl_base.pd()
+		self.ttl_base.color(*self.COL_ON)
+		# self.ttl_base.pensize(old_size)
+		self.ttl_base.fill(True)
+
+		self.ttl_base.seth(0)
+		self.ttl_base.fd(self.WD)
+		self.ttl_base.rt(90)
+		self.ttl_base.fd(self.HT)
+		self.ttl_base.rt(90)
+		self.ttl_base.fd(self.WD)
+		self.ttl_base.rt(90)
+		self.ttl_base.fd(self.HT)
+		self.ttl_base.rt(90)
+
+		self.ttl_base.fill(False)
+		# self.ttl_base.pensize(old_size)
+		self.ttl_base.color(*old_color)
+		self.ttl_base.pu()
+
+		self.write()
+
+
+	def off(self):
+		self.ttl_lit.clear()
