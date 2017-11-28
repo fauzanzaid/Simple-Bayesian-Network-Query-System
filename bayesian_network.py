6465f3eb1f3bf277960f80d8aabaffb234fbcaee
fauzanzaid <fauzanzaid.dev@gmail.com>
2017-11-28 14:16:46 +0530

    Implement recursive evaluation function

--- bayesian_network.py
+++ bayesian_network.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python2
 
 import re
+from operator import mul
 
 class BayesianNetwork(object):
 	"""docstring for BayesianNetwork"""
@@ -39,6 +40,22 @@ class BayesianNetwork(object):
 				self.nodes_by_name[k].parents.append(self.nodes_by_name[pn])
 				self.nodes_by_name[pn].children.append(self.nodes_by_name[k])
 
+		for n in self.nodes:
+			cpt_list = n.cpt_list
+			for i,cp in enumerate(cpt_list):
+				binary = format(i, "0"+str(len(n.parents))+"b")
+				event_values = [int(v) for v in list(binary)]
+
+				events_query_0 = [Event(n,0)]
+				events_query_1 = [Event(n,1)]
+				events_evidence = []
+				for j,p in enumerate(n.parents):
+					events_evidence.append(Event(p,event_values[j]))
+				prob_0 = Probability(events_query_0, events_evidence)
+				prob_1 = Probability(events_query_1, events_evidence)
+
+				n.cpt[prob_0] = 1-float(cp)
+				n.cpt[prob_1] = float(cp)
 
 
 class BayesianNetworkNode(object):
@@ -48,6 +65,8 @@ class BayesianNetworkNode(object):
 		self.parents = []
 		self.children = []
 		self.cpt_list = None
+		self.cpt = {}
+
 
 	def get_markov_blanket(self):
 		markov_blanket = []
@@ -64,6 +83,23 @@ class BayesianNetworkNode(object):
 		return markov_blanket
 
 
+	def get_all_parents(self):
+		all_parents = set(self.parents)
+		for n in iter(all_parents):
+			all_parents = all_parents | n.get_all_parents()
+		return all_parents
+
+
+	def __hash__(self):
+		return hash((self.name))
+
+	def __eq__(self, other):
+		return (self.name) == (other.name)
+
+	def __ne__(self, other):
+		return not(self==other)
+
+
 
 class Event(object):
 	"""docstring for Event"""
@@ -72,12 +108,28 @@ class Event(object):
 		self.value = value
 
 
+	def __hash__(self):
+		return hash((self.node, self.value))
 
-class ConditionalProbability(object):
-	"""docstring for ConditionalProbability"""
+	def __eq__(self, other):
+		return (self.node, self.value) == (other.node, other.value)
+
+	def __ne__(self, other):
+		return not(self==other)
+
+
+class Probability(object):
+	"""docstring for Probability"""
 	def __init__(self, events_query, events_evidence=[]):
-		self.events_query = events_query
-		self.events_evidence = events_evidence
+		self.events_query = frozenset(events_query)
+		self.events_evidence = frozenset(events_evidence)
+		self.events_query_by_node = {}
+		self.events_evidence_by_node = {}
+
+		for event in self.events_query:
+			self.events_query_by_node[event.node] = event
+		for event in self.events_evidence:
+			self.events_evidence_by_node[event.node] = event
 
 
 	def to_string(self):
@@ -113,11 +165,119 @@ class ConditionalProbability(object):
 
 
 	def evaluate(self, bn):
-		return 0
+
+		nodes_common = [n for n in self.events_query_by_node if n in self.events_evidence_by_node]
+		for n in nodes_common:
+			if self.events_query_by_node[n].value != self.events_evidence_by_node[n].value:
+				return 0
+
+		if len(self.events_query) == 0:
+			# Null event
+			return 0
+		
+		if len(self.events_query) == 1:
+			event = next(iter(self.events_query))	# Get the only set element
+			if self in event.node.cpt:
+				# Value exists in CPT
+				return event.node.cpt[self]
+		
+		if len(self.events_evidence) == 0:
+			# Chain rule and Marginalization
+			
+			# Find all affecting nodes
+			nodes_all = set()
+			for event in self.events_query:
+				nodes_all |= {event.node}
+				nodes_all |= event.node.get_all_parents()
+
+			events_by_node = {}
+			for node in nodes_all:
+				if node in self.events_query_by_node:
+					events_by_node[node] = self.events_query_by_node[node]
+				else:
+					events_by_node[node] = Event(node, None)
+
+			chain_uninst = []
+			for node in nodes_all:
+				events_query = [events_by_node[node]]
+				events_evidence = []
+				for p in node.parents:
+					events_evidence.append(events_by_node[p])
+				chain_uninst.append(Probability(events_query, events_evidence))
+
+			uninst_nodes = self.get_uninst_nodes_from_chain(chain_uninst)
+
+			chains_inst = []
+			for i in xrange(2**(len(uninst_nodes))):
+				binary = format(i, "0"+str(len(uninst_nodes))+"b")
+				event_values = [int(v) for v in list(binary)]
+				chains_inst.append(self.get_chain_inst_from_node_values(chain_uninst, dict(zip(uninst_nodes, event_values))))
+
+			products = []
+			for chain in chains_inst:
+				products.append(reduce(mul, [prob.evaluate(bn) for prob in chain], 1))
+
+			return sum(products)
+
+
+		else:
+			# Product rule
+			events_query_num = self.events_query | self.events_evidence
+			events_query_den = self.events_evidence
+			val_num = Probability(events_query_num).evaluate(bn)
+			val_den = Probability(events_query_den).evaluate(bn)
+			return val_num/float(val_den)
+
+
+	def get_uninst_nodes_from_chain(self, chain):
+		uninst_nodes = set()
+		for prob in chain:
+			for event in prob.events_query | prob.events_evidence:
+				if event.value == None:
+					uninst_nodes |= {event.node}
+		return uninst_nodes
+
+
+	def get_chain_inst_from_node_values(self, chain_uninst, values_by_node):
+		chain_inst = []
+		for prob_uninst in chain_uninst:
+			
+			events_query_inst = []
+			for event in prob_uninst.events_query:
+				if event.value == None:
+					# Event uninstantiated
+					events_query_inst.append(Event(event.node, values_by_node[event.node]))
+				else:
+					# Event already instantiated
+					events_query_inst.append(event)
+			
+			events_evidence_inst = []
+			for event in prob_uninst.events_evidence:
+				if event.value == None:
+					# Event uninstantiated
+					events_evidence_inst.append(Event(event.node, values_by_node[event.node]))
+				else:
+					# Event already instantiated
+					events_evidence_inst.append(event)
+
+			prob_inst = Probability(events_query_inst, events_evidence_inst)
+			chain_inst.append(prob_inst)
+
+		return chain_inst
+
+
+	def __hash__(self):
+		return hash((self.events_query, self.events_evidence))
+
+	def __eq__(self, other):
+		return (self.events_query, self.events_evidence) == (other.events_query, other.events_evidence)
+
+	def __ne__(self, other):
+		return not(self==other)
 
 
 
-def get_cond_prob_from_names(bn, names_qry, names_cond):
+def get_prob_from_names(bn, names_qry, names_cond):
 	events_query = []
 	events_evidence = []
 
@@ -131,7 +291,7 @@ def get_cond_prob_from_names(bn, names_qry, names_cond):
 			value = 1
 		event = Event(bn.nodes_by_name[name], value)
 		events_query.append(event)
-	
+
 	for name in names_cond:
 		value = None
 		if name[0] == "~":
@@ -143,5 +303,5 @@ def get_cond_prob_from_names(bn, names_qry, names_cond):
 		event = Event(bn.nodes_by_name[name], value)
 		events_evidence.append(event)
 
-	cp = ConditionalProbability(events_query, events_evidence)
-	return cp
+	p = Probability(events_query, events_evidence)
+	return p
